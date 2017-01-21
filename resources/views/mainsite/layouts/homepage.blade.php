ef4349be633f8ef680682f4f5aef5645f871ce92
Fan Lei <leifan833@msn.com>
2017-01-22 00:46:45 +0800

    post layout developed

--- resources/views/mainsite/layouts/homepage.blade.php
+++ resources/views/mainsite/layouts/homepage.blade.php
@@ -44,70 +44,103 @@
 @stop
 
 @section('content')
-  <div class="container-fluid tile-container">
+  <div class="container tile-container">
     <div class="row">
-      <div class='full-tile column col-xs-12'>
+
+      <div class='full-tile first-layer col-xs-12'>
         <span class="full-title">～最新美味内容～</span>
         <div class="rounded-line"></div>
       </div>
-
-      <div class='tile-grid'>
-
-        <div class='tile column col-xs-6 col-sm-4 col-md-3'>
+        {{--
+        <div class='tile col-xs-12 col-sm-6'>
           <div class="tile-box">
-            <figure class="effect-dexter">
-            <img src="/assets/img/19.jpg" alt="img19"/>
+          <figure class="effect-steve">
+            <img style="background-image: url('/upload/image/post_pic_default.jpg'); height: 356px;" />
             <figcaption>
-              <h2>Strange <span>Dexter</span></h2>
-              <p>Dexter had his own strange way. You could watch him training ants.</p>
+              <h2>我爱吃 <span>炸酱面</span></h2>
+              <p>大家好我最爱吃炸酱面了！</p>
               <a href="#">View more</a>
             </figcaption>     
           </figure>
           </div>
         </div>
 
-        <div class='tile column col-xs-6 col-sm-4 col-md-3'>
+        <div class='tile col-xs-12 col-sm-6'>
           <div class="tile-box">
-            <figure class="effect-dexter">
-            <img src="/assets/img/19.jpg" alt="img19"/>
+          <figure class="effect-steve">
+            <img style="background-image: url('/upload/image/7_image.jpg'); height: 356px;" />
             <figcaption>
-              <h2>Strange <span>Dexter</span></h2>
-              <p>Dexter had his own strange way. You could watch him training ants.</p>
+              <h2>Liong <span>小鲜肉</span></h2>
+              <p>裸奔的小鲜肉。</p>
               <a href="#">View more</a>
             </figcaption>     
           </figure>
           </div>
         </div>
-
-        <div class='tile column col-xs-6 col-sm-4 col-md-3'>
+        --}}
+        
+        @foreach ($posts as $post)
+        <div class='tile col-xs-12 col-sm-6'>
           <div class="tile-box">
-            <figure class="effect-dexter">
-            <img src="/assets/img/19.jpg" alt="img19"/>
+          <figure class="effect-steve">
+            <img style="background-image: url('{{ page_image($post->page_image) }}'); height: 356px;" />
             <figcaption>
-              <h2>Strange <span>Dexter</span></h2>
-              <p>Dexter had his own strange way. You could watch him training ants.</p>
-              <a href="#">View more</a>
+              <h2>{{ $post->title }}</h2>
+              <p>
+                @if ($post->subtitle)
+                  {{ $post->subtitle }}
+                @endif
+              </p>
+              <a href="{{ $post->url($tag) }}">View more</a>
             </figcaption>     
           </figure>
           </div>
         </div>
+        @endforeach
 
-        <div class='tile column col-xs-6 col-sm-4 col-md-3'>
-          <div class="tile-box">
-            <figure class="effect-dexter">
-            <img src="/assets/img/19.jpg" alt="img19"/>
-            <figcaption>
-              <h2>Strange <span>Dexter</span></h2>
-              <p>Dexter had his own strange way. You could watch him training ants.</p>
-              <a href="#">View more</a>
-            </figcaption>     
-          </figure>
-          </div>
-        </div>
+      <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+        {{-- 分页 --}}
+        <ul class="pager">
 
-
+          {{-- Reverse direction --}}
+          @if ($reverse_direction)
+            @if ($posts->currentPage() > 1)
+              <li class="previous">
+                <a href="{!! $posts->url($posts->currentPage() - 1) !!}">
+                  <i class="fa fa-long-arrow-left fa-lg"></i>
+                  Previous {{ $tag->tag }} Posts
+                </a>
+              </li>
+            @endif
+            @if ($posts->hasMorePages())
+              <li class="next">
+                <a href="{!! $posts->nextPageUrl() !!}">
+                  Next {{ $tag->tag }} Posts
+                  <i class="fa fa-long-arrow-right"></i>
+                </a>
+              </li>
+            @endif
+          @else
+            @if ($posts->currentPage() > 1)
+              <li class="previous">
+                <a href="{!! $posts->url($posts->currentPage() - 1) !!}">
+                  <i class="fa fa-long-arrow-left fa-lg"></i>
+                  Newer {{ $tag ? $tag->tag : '' }} Posts
+                </a>
+              </li>
+            @endif
+            @if ($posts->hasMorePages())
+              <li class="next">
+                <a href="{!! $posts->nextPageUrl() !!}">
+                  Older {{ $tag ? $tag->tag : '' }} Posts
+                  <i class="fa fa-long-arrow-right"></i>
+                </a>
+              </li>
+            @endif
+          @endif
+        </ul>
       </div>
-
+      
     </div>
   </div>
 @stop
