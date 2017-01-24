af59dc8f654a177afbdb7a6a612b6e6f2b53962c
Fan Lei <leifan833@msn.com>
2017-01-25 00:39:46 +0800

    first online version

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Be right back.</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato', sans-serif;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">404</div>
+                <div class="title">Ooops~您来到了不存在的页面</div>
+                <p>
+                    <a href="/">返回首页</a>
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
